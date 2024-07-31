71cd8fd02dab47c2c0bc85a91be0b5f5b8b5a251
Lorenzo Tellez <ltellez@gallopintonetworks.net>
2024-07-30 18:15:36 -0600

    Primer reto

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Python/resolucon.py
@@ -0,0 +1,23 @@
+#https://www.python.org/
+
+#Comentario en una linea 
+
+"""
+Esto es un comentario en varias linea
+"""
+'''
+Esto tambien es valido
+'''
+
+mi_variable= 2
+
+MY_CONSTANT = "Mi constante" #por convensión
+
+my_int: int = 10
+my_string="Hola mundo"
+my_float = 10.1
+my_bool = True
+
+print("Hola Python")
+
+print(type(mi_variable))
